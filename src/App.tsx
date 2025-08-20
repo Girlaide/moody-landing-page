6cd75df493d481aa8eab74fecea126722b3368dd
Girlaide Dev1225 <girlaide30@gmail.com>
2025-08-19 21:37:02 -0300

    import Button from './components/Button';

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Layout } from './components/Layout';
 import { Hero } from './components/Hero';
+import { Button } from './components/Button';
 
 export function App() {
   return (
     <Layout>
       <Hero />
+      <Button />
     </Layout>
   );
 }
