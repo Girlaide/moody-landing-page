d567d7d74f59e7b7c39b427f617f55a97751ec9b
Girlaide Dev1225 <girlaide30@gmail.com>
2025-08-25 18:56:36 -0300

    backgroundImage

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,19 @@
 import { Button } from './Button';
-
+import HeroImg from '../assets/background.png';
 export function Hero() {
   return (
-    <>
-      <h2 className="text-red-500">Exemplo!</h2>
-    </>
+    <section>
+      <div className="container grid grid-cols-1 md:*:grid-cols-2 min-h-[650] relative">
+        <div className="flex flex-col justify-center py-14 md:py-0 ">
+          <div className="flex justify-center items-center">
+            <img
+              src={HeroImg}
+              alt="hero-image"
+              className="w-[350px] h-[750px] md:w-[450px] md:h-[650px] xl:w-[9100px] ps-5 pl-2 pr-2  p-15  "
+            />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
