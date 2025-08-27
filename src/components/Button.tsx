193678ed65bfb999b95ce2bb53ceb84fe0b1571e
Maria Luiza <malumonteiro.dev@gmail.com>
2025-08-26 21:13:24 -0300

    fix: separa classes de bg e text (#5)

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 export function Button({ children, className, ...props }: ButtonProps) {
   return (
     <button
-      className={`bg-whitetext-black px-8 py-2 rounded-full border border-black/10 hover:bg-gray-100 transition-colors duration-200 font-bold
+      className={`bg-white text-black px-8 py-2 rounded-full border border-black/10 hover:bg-gray-100 transition-colors duration-200 font-bold
         ${className}
       `.trim()}
       {...props}
